1975a4282841f658ac2e8af16d0c8a3bb0e77ce2
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-03-31 20:03:08 +0530

    Update TypewiseAlert.cs

--- TypewiseAlert/TypewiseAlert.cs
+++ TypewiseAlert/TypewiseAlert.cs
@@ -1,159 +1,179 @@
 using System;
+using System.Collections.Generic;
+using static TypewiseAlert.TypewiseAlert;
 
 namespace TypewiseAlert
 {
-  public class TypewiseAlert
-  {
-    public enum BreachType {
-      NORMAL,
-      TOO_LOW,
-      TOO_HIGH
-    };
-    
-    public enum CoolingType
-    {
-        PASSIVE_COOLING,
-        HI_ACTIVE_COOLING,
-        MED_ACTIVE_COOLING
-    };
+    public class TypewiseAlert
+    {       
+        public enum BreachType {
+            NORMAL,
+            TOO_LOW,
+            TOO_HIGH
+        };
+        public enum CoolingType
+        {
+            PASSIVE_COOLING,
+            HI_ACTIVE_COOLING,
+            MED_ACTIVE_COOLING
+        };
 
-    public enum AlertTarget
-    {
-        TO_CONTROLLER,
-        TO_EMAIL
-    };
+        public enum AlertTarget
+        {
+            TO_CONTROLLER,
+            TO_EMAIL
+        };
 
-  public static BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-      if(value < lowerLimit) {
-        return BreachType.TOO_LOW;
-      }
-      if(value > upperLimit) {
-        return BreachType.TOO_HIGH;
-      }
-      return BreachType.NORMAL;
-    }
+        public class CoolingLimitDictionaryInitializer
+        {
+            public Dictionary<CoolingType, Func<ILimitInitializer>> _CoolingLimitType = new Dictionary<CoolingType, Func<ILimitInitializer>>();
 
-    public static BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
-      ExtremeLimit _extremeLimit = new PassiveCoolingLimit().SetExtremeLimit(coolingType);
-      return inferBreach(temperatureInC, _extremeLimit.lowerLimit, _extremeLimit.upperLimit);
-    }
+            public CoolingLimitDictionaryInitializer()
+            {
+                _CoolingLimitType.Add(CoolingType.PASSIVE_COOLING, () => { return new PassiveCoolingLimit(); });
+                _CoolingLimitType.Add(CoolingType.HI_ACTIVE_COOLING, () => { return new HighCoolingLimit(); });
+                _CoolingLimitType.Add(CoolingType.MED_ACTIVE_COOLING, () => { return new MediumCoolingLimit(); });
+            }
+        }
 
-    class ExtremeLimit
-    {
-        public int lowerLimit { get; set; }
-        public int upperLimit { get; set; }
-    }
-    
-    interface ILimitInitializer
-    {
+        public class EmailMessageInitializer
+        {
+            public Dictionary<BreachType, Func<IEmailTrigger>> _Email = new Dictionary<BreachType, Func<IEmailTrigger>>();
+
+            public EmailMessageInitializer()
+            {
+                _Email.Add(BreachType.TOO_HIGH, () => { return new HighLimitMessageEmail(); });
+                _Email.Add(BreachType.TOO_LOW, () => { return new LowLimitMessageEmail(); });
+                _Email.Add(BreachType.NORMAL, () => { return new NormalLimitMessageEmail(); });
+            }
+        }
+
+        public class AlertTargetType
+        {
+            public Dictionary<AlertTarget, String> _AlertTargetType = new Dictionary<AlertTarget, String>();
+            public AlertTargetType(BreachType breachType)
+            {
+                _AlertTargetType.Add(AlertTarget.TO_CONTROLLER, sendToController(breachType));
+                _AlertTargetType.Add(AlertTarget.TO_EMAIL, sendToEmail(breachType));
+            }
+        }
+
+        public static BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+        if (value < lowerLimit) {
+            return BreachType.TOO_LOW;
+        }
+        if (value > upperLimit) {
+            return BreachType.TOO_HIGH;
+        }
+        return BreachType.NORMAL;
+        }
+
+        public static BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) 
+        {
+          ExtremeLimit _extremeLimit = new CoolingLimitDictionaryInitializer()._CoolingLimitType[coolingType]().SetExtremeLimit(coolingType);
+          return inferBreach(temperatureInC, _extremeLimit.lowerLimit, _extremeLimit.upperLimit);
+        }
+
+        public class ExtremeLimit
+        {
+            public int lowerLimit { get; set; }
+            public int upperLimit { get; set; }
+        }
+
+        public interface ILimitInitializer
+        {
             ExtremeLimit SetExtremeLimit(CoolingType coolingType);
-    }
+        }
 
-    class PassiveCoolingLimit : ILimitInitializer
-    {
-        public ExtremeLimit SetExtremeLimit(CoolingType coolingType)
+        class PassiveCoolingLimit : ILimitInitializer
         {
-            return new ExtremeLimit()
+            public ExtremeLimit SetExtremeLimit(CoolingType coolingType)
             {
-                lowerLimit = 0,
-                upperLimit = 35
-            };
+                return new ExtremeLimit()
+                {
+                    lowerLimit = 0,
+                    upperLimit = 35
+                };
+            }
         }
-    }
 
-    class HighCoolingLimit : ILimitInitializer
-    {
-        public ExtremeLimit SetExtremeLimit(CoolingType coolingType)
+        class HighCoolingLimit : ILimitInitializer
         {
-            return new ExtremeLimit()
+            public ExtremeLimit SetExtremeLimit(CoolingType coolingType)
             {
-                lowerLimit = 0,
-                upperLimit = 45
-            };
+                return new ExtremeLimit()
+                {
+                    lowerLimit = 0,
+                    upperLimit = 45
+                };
+            }
         }
-    }
 
-    class MediumCoolingLimit : ILimitInitializer
-    {
-        public ExtremeLimit SetExtremeLimit(CoolingType coolingType)
+        class MediumCoolingLimit : ILimitInitializer
         {
-            return new ExtremeLimit()
+            public ExtremeLimit SetExtremeLimit(CoolingType coolingType)
             {
-                lowerLimit = 0,
-                upperLimit = 35
-            };
+                return new ExtremeLimit()
+                {
+                    lowerLimit = 0,
+                    upperLimit = 35
+                };
+            }
         }
-    }
 
-    public struct BatteryCharacter {
-      public CoolingType coolingType;
-      public string brand;
-    }
-    
-    public static void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) 
-    {
-      BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
+        public struct BatteryCharacter {
+          public CoolingType coolingType;
+          public string brand;
+        }
 
-      switch(alertTarget) {
-        case AlertTarget.TO_CONTROLLER:
-          sendToController(breachType);
-          break;
-        case AlertTarget.TO_EMAIL:
-          sendToEmail(breachType);
-          break;
-      }
-    }
-    
-    public static void sendToController(BreachType breachType) {
-      const ushort header = 0xfeed;
-      Console.WriteLine("{} : {}\n", header, breachType);
-    }
-    
-    public static void sendToEmail(BreachType breachType) {
-      string recepient = "a.b@c.com";
-      switch(breachType) {
-        case BreachType.TOO_LOW:
-          Console.WriteLine("To: {}\n", recepient);
-          Console.WriteLine("Hi, the temperature is too low\n");
-          break;
-        case BreachType.TOO_HIGH:
-          Console.WriteLine("To: {}\n", recepient);
-          Console.WriteLine("Hi, the temperature is too high\n");
-          break;
-        case BreachType.NORMAL:
-          break;
-      }
-    }
-    interface IEmailTrigger
-    {
-         void TriggerEmail(string Recepient, BreachType _BreachType);
-    }
-
-    class LowLimitMessageEmail : IEmailTrigger
-    {
-        public void TriggerEmail(string Recepient, BreachType _BreachType)
+        public static void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) 
         {
-            Console.WriteLine("To: {}\n", Recepient);
-            Console.WriteLine("Hi, the temperature is too low\n");
+          BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
+          string result = new AlertTargetType(breachType)._AlertTargetType[alertTarget];
         }
-    }
 
-    class HighLimitMessageEmail : IEmailTrigger
-    {
-        public void TriggerEmail(string Recepient, BreachType _BreachType)
-        {
-            Console.WriteLine("To: {}\n", Recepient);
-            Console.WriteLine("Hi, the temperature is too high\n");
+        public static String sendToController(BreachType breachType) {
+          const ushort header = 0xfeed;
+          Console.WriteLine("{} : {}\n", header, breachType);
+          return "Done";
         }
-    }
 
-    class NormalLimitMessageEmail : IEmailTrigger
-    {
-        public void TriggerEmail(string Recepient, BreachType _BreachType)
+        public static String sendToEmail(BreachType breachType) {
+          string recepient = "a.b@c.com";
+          new EmailMessageInitializer()._Email[breachType]().TriggerEmail(recepient, breachType);
+          return "Done";
+        }
+
+        public interface IEmailTrigger
         {
-            Console.WriteLine("To: {}\n", Recepient);
-            Console.WriteLine("Hi, the temperature is in normal state\n");
+             void TriggerEmail(string Recepient, BreachType _BreachType);
+        }
+
+        class LowLimitMessageEmail : IEmailTrigger
+        {
+            public void TriggerEmail(string Recepient, BreachType _BreachType)
+            {
+                Console.WriteLine("To: {}\n", Recepient);
+                Console.WriteLine("Hi, the temperature is too low\n");
+            }
+        }
+
+        class HighLimitMessageEmail : IEmailTrigger
+        {
+            public void TriggerEmail(string Recepient, BreachType _BreachType)
+            {
+                Console.WriteLine("To: {}\n", Recepient);
+                Console.WriteLine("Hi, the temperature is too high\n");
+            }
+        }
+
+        class NormalLimitMessageEmail : IEmailTrigger
+        {
+            public void TriggerEmail(string Recepient, BreachType _BreachType)
+            {
+                Console.WriteLine("To: {}\n", Recepient);
+                Console.WriteLine("Hi, the temperature is in normal state\n");
+            }
         }
     }
-  }
 }
