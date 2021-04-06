315e991754e0beedc86ac22e1379f21e6a274522
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-04-06 14:35:13 +0530

    Update TypewiseAlertTest.cs

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -9,6 +9,8 @@ namespace TypewiseAlert.Test
     public void InfersBreachAsPerLimits()
     {
        Assert.True(TypewiseAlert.InferBreach(12, 20, 30) == TypewiseAlert.BreachType.TOO_LOW);
+       Assert.True(TypewiseAlert.InferBreach(35, 20, 30) == TypewiseAlert.BreachType.TOO_HIGH);
+       Assert.True(TypewiseAlert.InferBreach(22, 20, 30) == TypewiseAlert.BreachType.NORMAL);
     }
   }
 }
