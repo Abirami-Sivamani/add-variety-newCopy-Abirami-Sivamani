4cce70b070015442395aaaa10a1132f69b8bb79f
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-04-15 11:16:51 +0530

    Update TypewiseAlert.cs

--- TypewiseAlert/TypewiseAlert.cs
+++ TypewiseAlert/TypewiseAlert.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using TypewiseAlert.Interfaces;
 using TypewiseAlert.Notifiers;
 using static TypewiseAlert.TypewiseAlert;
